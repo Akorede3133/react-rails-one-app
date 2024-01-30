55393768fbbedb19473b9f90b874cf58f9a6ecf8
akorede <emiolasaheed1@gmail.com>
2024-01-30 19:46:53 +0100

    Create greetings table and fill with data

---
+++ db/migrate/20240130184104_greeting.rb
@@ -0,0 +1,4 @@
+class Greeting < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Clear existing data
+Greeting.destroy_all
+
+# Create five different greetings
+greetings = [
+  "Hello, welcome to our website!",
+  "Greetings from the team!",
+  "Good day! How can we assist you today?",
+  "Welcome aboard! Enjoy your stay.",
+  "Hi there! Thanks for visiting."
+]
+
+greetings.each do |message|
+  Greeting.create(message: message)
+end
+
+puts "Seed data has been successfully created!"
