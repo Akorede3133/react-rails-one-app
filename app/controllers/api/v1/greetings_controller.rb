973effd1df97dea9863123c8e49ea9912fd2f113
akorede <emiolasaheed1@gmail.com>
2024-01-31 17:23:18 +0100

    fix linters errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::GreetingsController < ApplicationController 
+class Api::V1::GreetingsController < ApplicationController
   def index
     @greeting = Greeting.all.sample
     render json: @greeting, only: :message
